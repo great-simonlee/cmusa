fba50a4692c0d3848d8737354a9801426ee01ee0
Simon Lee <great.simonlee@gmail.com>
2022-08-27 14:17:59 -0400

    header and footer

--- footer.php
+++ footer.php
@@ -1,5 +1,58 @@
-    <h1 style="background-color: green">THIS IS FOOTER</h1>
-    <h1>This is a footer.</h1>
+        <!-- china moves logo(white w_ trans)_937x97.png -->
+        <footer id="gfter">
+            <section class="footer-con">
+                <div class="footer-cont-container">
+                    <div class="footer-info">
+                        <img class="footer-logo" src="<?php echo get_theme_file_uri('/assets/img/logo/china moves logo(white w_ trans)_937x97.png') ?>" alt="qr">
+                        <p>1133 Broadway, #401, New York, NY 10010</p>
+                        <p>+1 212 680 4552</p>
+                        <p>hello@newyorkmovesre.com</p>
+                    </div>
+                    <div class="footer-list-container">
+                        <ul>
+                            <li><b>关于公司</b></li>
+                            <li><a href="/aboutus">关于我们</a></li>
+                            <li><a href="">联系我们</a></li>
+                            <li><a href="/team">加入我们</a></li>
+                        </ul>
+                        <ul>
+                            <li><b>地产服务</b></li>
+                            <li><a href="/rent">租房信息</a></li>
+                            <li><a href="/roommate">室友服务</a></li>
+                        </ul>
+                        <ul>
+                            <li><b>其他信息</b></li>
+                            <li><a href="">常见问题</a></li>
+                            <li><a href="">买房咨询</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section class="footer-pri">
+                <div class="footer-cont-pri">
+                    <p>Copyright NEW YORK MOVES REAL ESTATE. All Rights Reserved.</p>
+                    <p>版权归本网站所有</p>
+                </div>
+            </section>
+        </footer>
+
+        <!-- Firebase JS -->
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-auth.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-firestore.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-storage.js"></script>
+        <!-- Google API -->
+        <script src="https://apis.google.com/js/api.js"></script>
+        <!-- MAPBOX JS -->
+        <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
+        <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JlYXQtc2ltb25sZWUiLCJhIjoiY2t6b2szZ2U5NjA0ZjJwdHZiY3Q1OHNtcyJ9.qdLhsMCScxxfzT9CYywRUQ';
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js"></script>
+        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js"></script>
+        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css" type="text/css">
+        <!-- EMAIL JS -->
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
 
     <?php wp_footer(); ?>
   </body>
--- header.php
+++ header.php
@@ -1,12 +1,52 @@
 <!DOCTYPE html>
-    <html <?php language_attributes(); ?>>
-        <head>
-            <meta charset="<?php bloginfo('charset'); ?>">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <?php wp_head();?>
-        </head>
-        <body <?php body_class();?>>
-        <h1 style="background-color: tomato">THIS IS HEADER</h1>
-
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head();?>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
+    </head>
+    <body <?php body_class();?>>
+      <nav class="gnav" id="gnav">
+        <section class="gnav-bar">
+          <a href="<?php echo site_url('/') ?>" class="gnav-title"><img class="gnav-logo" src="<?php echo get_theme_file_uri('/assets/img/logo/china moves logo(white w_ trans)_937x97.png') ?>" alt="ChinaMovesUSA"></a>
+          <ul class="gnav-ul">
+            <li><a href="/">首页</a></li>
+            <li><a href="/rent">租房</a></li>
+            <li><a href="/roommate">找室友</a></li>
+            <li><a href="/team">团队</a></li>
+            <li><a href="/aboutus">关于我们</a></li>
+            <li><a href="/register">发布房源</a></li>
+          </ul>
+          <div class="login-signup" id="signUpLogInDiv">
+            <a href="/member-login">登陆</a>
+            <a href="/member-signup" class="gnav-signup">注册</a>
+          </div>
+          <div class="user-menu-cont" id="userMenuDiv">
+            <img class="user-menu" id="userMenu" src="<?php echo get_theme_file_uri('/assets/img/icon/userMenu.png') ?>">
+            <div class="dd-menu-cont" id="dd-menu-cont" style="display: none;">
+              <div class="dd-menu-item"><a href="<?php echo site_url('/mypage') ?>">我的账户</a></div>
+              <div class="dd-menu-item"><a href="<?php echo site_url('/mypage?mylist') ?>">我的房源</a></div>
+              <div class="dd-menu-item" id="logOutBtn">登出</div>
+            </div>
+            <!-- <img class="user-menu" src="<?php echo get_theme_file_uri('/assets/img/icon/userMenuLine.png') ?>"> -->
+            <!-- <a class="gnav-signup" id="logOutBtn">LOGOUT</a> -->
+          </div>
+        </section>
+      </nav>
+      <nav class="m-header">
+        <div class="mh-space"></div>
+        <div class="mh-nav">
+          <div class="mh-city">
+            <img class="mh-loc-img" src="<?php echo get_theme_file_uri('/assets/img/mobile/m-location.png') ?>" alt="ChinaMovesUSA">
+            <p>选择城市</p>
+            <img class="mh-dtri-img" src="<?php echo get_theme_file_uri('/assets/img/mobile/m-downtri.png') ?>" alt="ChinaMovesUSA">
+          </div>
+          <img class="mh-logo-img" src="<?php echo get_theme_file_uri('/assets/img/logo/china moves logo(white w_ trans)_937x97.png') ?>" alt="ChinaMovesUSA">
+          <div class="mh-menu">
+            <img class="mh-menu-img" src="<?php echo get_theme_file_uri('/assets/img/mobile/m-menu.png') ?>" alt="ChinaMovesUSA">
+          </div>
+        </div>
+      </nav>
 
