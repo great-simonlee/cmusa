1ea471bc4aac46631c15976c6b8845df48e36a49
Simon Lee <great.simonlee@gmail.com>
2022-08-27 18:27:01 -0400

    Mobile page redirect has been made

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7e23033160b4a9662389');
+<?php return array('dependencies' => array(), 'version' => '4d2388e43220af073a7b');
--- src/modules/front.js
+++ src/modules/front.js
@@ -1,4 +1,13 @@
 if (window.location.pathname === '/') {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // Take the user to a different screen here.
+    window.location = '/mobile';
+  }
+
   window.addEventListener('DOMContentLoaded', () => {
     const db = firebase.firestore();
     const auth = firebase.auth();
